9c9b2fb48a9c9fe6373033d5386356c27acbc29e
sharyar <131683156+sharyarnaveed@users.noreply.github.com>
2024-07-20 04:44:50 +0500

    quiz app home done

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Navbar from './components/navbar.vue';
+</script>
+
+<template>
+  <!-- <Navbar/> -->
+
+  <RouterView />
+</template>
+
+
---
+++ src/components/createquiz.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '../assets/home.css';
+
+
+
+</script>
+
+<template>
+    <main class="createquiz_section">
+
+        
+        
+    </main>
+</template>
