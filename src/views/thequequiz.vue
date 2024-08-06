412aee819909201d1a9bec314a44eee21b0df4f5
sharyar <131683156+sharyarnaveed@users.noreply.github.com>
2024-08-06 12:11:38 +0500

    start quiz complete

--- src/views/thequequiz.vue
+++ src/views/thequequiz.vue
@@ -1,15 +1,17 @@
 <script setup>
-import '../assets/startquiz.css';
+// import '../assets/startquiz.css';
 </script>
 
 
 <template>
-  <main    >
- 
+  
+ <main class="thequemain">
  <section class="quizcon_inmain">
    <div class="top">
      <h1>Title</h1>
    </div>
+
+
    <div class="info">
  
      <div class="input-wrapper">
@@ -34,14 +36,153 @@ import '../assets/startquiz.css';
  
  
    </div>
+
+
  <div class="quizque">
-   que
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur repellendus earum incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem itaque sed, beatae rerum soluta facilis quibusdam fuga libero, tenetur animi cum pariatur dignissimos eaque modi in quidem delectus inventore, consequatur sequi iusto.</p>
+   <div class="optionofquiz">
+    <p><input type="radio" name="1" id=""> Ans 1 </p>
+    <p><input type="radio" name="1" id=""> Ans2</p>
+    <p><input type="radio" name="1" id="">ans3 </p>
+    <p><input type="radio" name="1" id=""> ans4 </p>
+   </div>
  </div>
+
  </section>
     
  
+</main>
  
  
- 
-   </main> 
+
 </template>
+
+
+<style scoped>
+
+.thequemain{
+  /* border: 2px solid red; */
+  height: 100vh;
+  widows: 100vh;
+  display: flex;
+  background-image: url("https://images.unsplash.com/photo-1722566351594-be51e61e1a4f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  background-size: contain;
+  justify-content: center;
+  padding-top: 5%;
+  
+}
+.quizcon_inmain{
+  /* border: 2px solid red; */
+  width: 80%;
+  background-color: white;
+  /* border-radius: 10px; */
+}
+.top{
+  /* border-top: 2px solid green; */
+  height: 20vh;
+  display: flex;
+  align-items: center;
+  padding: 0px 20px;
+  /* border-top-left-radius: 8px; */
+  /* border-top-right-radius: 8px; */
+  background-color: rgb(12, 11, 11);
+}
+.top h1
+{
+  font-size: 4rem;
+  margin: 2px 10px;
+  color: white;
+  font-family: var(--heading_font);
+}
+
+.info{
+    /* border: 2px solid orange; */
+    height: 17%;
+    padding: 10px 10px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  
+}
+.info div
+{
+    /* border: 2px solid blue; */
+    width: 35%;
+    display: flex;
+    align-items: center;
+    padding: 5px 7px;
+    height: 90%;
+    /* background-color: white; */
+    justify-content: space-evenly;
+}
+
+/* .info label input
+{
+   
+} */
+
+.input-wrapper {
+    --padding: 16px;
+    --focus-color: black;
+    position: relative;
+    display: flex;
+    align-items: center;
+  }
+  
+  .theinfo_input{
+    padding: var(--padding);
+    border: 2px solid gray;
+    outline: none;
+    width: 100%;
+    height: 99%;
+    padding: 0px 8px;
+    font-size: 18px;
+    outline: none;
+    border-radius: 6px;
+  }
+  .placeholder {
+    position: absolute;
+    background-color: white;
+    color: gray;
+    font-family: var(--heading_font);
+    left: var(--padding);
+    font-size: 16px;
+    padding: 0 4px;
+    transition: all 0.2s ease-in;
+  }
+  
+  .theinfo_input:is(:focus, :valid) {
+    border: 2px solid var(--focus-color);
+  }
+  
+  .theinfo_input:is(:focus, :valid) + .placeholder {
+    transform: translatey(calc(-1 * var(--padding) - 12px));
+    font-size: 16px;
+    color: var(--focus-color);
+    font-family: var(--heading_font);
+  }
+
+.quizque{
+  /* border: 2px solid purple; */
+  margin: 10px 5px;
+  padding: 5px 3%;
+  height: 180px;
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.optionofquiz{
+  /* border: 2px solid brown; */
+  height: 60%;
+  width: 95%;
+  display: grid;
+  grid-template-columns: repeat(2,auto);
+  align-items: center;
+}
+.optionofquiz p
+{
+  font-size: 18px;
+}
+</style>
